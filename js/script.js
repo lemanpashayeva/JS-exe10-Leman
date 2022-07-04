e696b6db8784fd33cf4f383e77f8fd03f8f45979
lemanpashayeva <lmnpasayeva@gmail.com>
2022-07-04 18:47:08 +0400

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
  //exe
-
- function calcTip(){
-    const amount = [125, 555, 44 ];
+ const amount = [125, 555, 44];
+ function calcTip1(){
     
     let x = amount[0];
     if ( x>50 & x<300 ){
         return x*15/100
     } else if ( x>200 ){
         return x*20/100
+    } else if ( x<50 ){
+        return x
     };
  };
 
  const tips = [];
- tips[0] = calcTip();
+ tips.push(calcTip1());
+
+
+ function calcTip2(){
+    let x = amount[1];
+    if ( x>50 & x<300 ){
+        return x*15/100
+    } else if ( x>200 ){
+        return x*20/100
+    } else if ( x<50 ){
+        return x
+    };
+ };
+
+ tips.push(calcTip2());
+
+
+ function calcTip3(){
+    let x = amount[2];
+    if ( x>50 & x<300 ){
+        return x*15/100
+    } else if ( x>200 ){
+        return x*20/100
+    } else if ( x<50 ){
+        return x
+    };
+ };
+
+ tips.push(calcTip3());
 
  console.log(tips)
